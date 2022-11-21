2229b854c1bb69f2853d993e5803f23270716c42
Agustin <austintv52@gmail.com>
2022-11-20 23:50:44 -0600

    Integración de plantilla, se integra modulo de reserva y estabilidad del sistema

---
+++ frontend/seccionWeb/home.php
@@ -0,0 +1,54 @@
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container">
+        <br><br><br><br><br>
+        <div class="masthead-subheading">Bienvenido a nuestro</div>        
+        <div class="masthead-heading text-uppercase">Hotel Casa de Piedra</div>        
+        <a class="btn btn-xl text-uppercase" style="background: #212529; color: aliceblue;" href="#services">Reservar ahora</a>
+    </div>
+</header>
+<!-- Services-->
+<section class="page-section" id="services">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Servicio de Hotel 24/7</h2>
+            <h3 class="section-subheading text-muted">Visita Casa de Piedra, conoce nuestras amenidades.</h3>
+        </div>
+        <div>
+            
+        </div>
+        <div class="row text-center">
+            <div class="col-md-4">
+                <!-- <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x "></i>
+                    <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                </span>                -->
+                <img src="BackWeb/assets/img/habitaciones/sencilla.jpg" class="card-img-top" alt="">
+                <h4 class="my-3">Habitación sencilla</h4>
+                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam
+                    architecto quo inventore harum ex magni, dicta impedit.</p>
+            </div>
+            <div class="col-md-4">
+                <!-- <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x "></i>
+                    <i class="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                </span> -->
+                <img src="BackWeb/assets/img/habitaciones/doble.jpg" class="card-img-top" alt="">
+                <h4 class="my-3">Habitación doble</h4>
+                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam
+                    architecto quo inventore harum ex magni, dicta impedit.</p>
+            </div>
+            <div class="col-md-4">
+                <!-- <span class="fa-stack fa-4x">
+                    <i class="fas fa-circle fa-stack-2x "></i>
+                    <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+                </span> -->
+                <img src="BackWeb/assets/img/habitaciones/premium.jpg" class="card-img-top" alt="">
+                <h4 class="my-3">Habitación premium</h4>
+                <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam
+                    architecto quo inventore harum ex magni, dicta impedit.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
